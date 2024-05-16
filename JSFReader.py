09c4a3ebce90119972bbaf09e9ebe1d31e40fef0
dunn-michael <dunnm2001@gmail.com>
2024-05-16 09:38:54 -0600

    commented out prints and set direct to sidescan

--- JSFReader.py
+++ JSFReader.py
@@ -15,12 +15,14 @@ header_struct = struct.Struct('>5siiiiiiffii')
 ping_struct = struct.Struct('>IIfiiiihhhhhhhhhh')
 
 # open the file in binary mode
-with open('Immerse/dataSet.JSF', 'rb') as f:
+with open('Immerse/SideScan 0/20240416-201317-UTC_0-2024-04-16_oahu_kohola_HFB5m_kuleana_spot_1-IVER3-3099_WP0.JSF', 'rb') as f:
     # read the header
     header = f.read(header_struct.size)
     magic_number, file_format_version, file_size, ping_count, first_ping_offset, \
     last_ping_size, sample_format, sample_rate, sonar_id, ping_period, \
     range_scale_factor = header_struct.unpack(header)
+
+    print(header)
 
     # read the pings
     for i in range(ping_count):
@@ -36,32 +38,32 @@ with open('Immerse/dataSet.JSF', 'rb') as f:
 
         # process the ping data
         # ...
-        print(f"{sonar_x}, {sonar_y}, {sonar_depth} \n\n")
+        # print(f"{sonar_x}, {sonar_y}, {sonar_depth} \n\n")
         x_coords.append(sonar_x)
         y_coords.append(sonar_y)
         z_coords.append(sonar_depth)
 
 
-# Creating data
-# x = np.linspace(-5, 5, 100)
-# y = np.linspace(-5, 5, 100)
-X, Y = np.meshgrid(x_coords, y_coords)
-# 
-# Z = np.sin(X) + np.cos(Y)
-Z = z_coords
+# # Creating data
+# # x = np.linspace(-5, 5, 100)
+# # y = np.linspace(-5, 5, 100)
+# X, Y = np.meshgrid(x_coords, y_coords)
+# # 
+# # Z = np.sin(X) + np.cos(Y)
+# Z = z_coords
 
-# Creating a 3D plot
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+# # Creating a 3D plot
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
 
-# Plotting the 3D contour
-ax.contour3D(X, Y, Z, 50, cmap='viridis')
+# # Plotting the 3D contour
+# ax.contour3D(X, Y, Z, 50, cmap='viridis')
 
-# Customizing the plot
-ax.set_xlabel('X-axis')
-ax.set_ylabel('Y-axis')
-ax.set_zlabel('Z-axis')
-ax.set_title('Basic 3D Contour Plot')
+# # Customizing the plot
+# ax.set_xlabel('X-axis')
+# ax.set_ylabel('Y-axis')
+# ax.set_zlabel('Z-axis')
+# ax.set_title('Basic 3D Contour Plot')
 
-# Displaying the plot
-plt.show()
+# # Displaying the plot
+# plt.show()
