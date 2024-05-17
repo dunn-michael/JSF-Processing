a2d756bc18a48580fcbc0062034263d96410d790
dunn-michael <dunnm2001@gmail.com>
2024-05-17 09:34:56 -0600

    Possibly Working, test on mac

--- anew.py
+++ anew.py
@@ -2,8 +2,16 @@ import struct
 import os
 
 
+
+# %matplotlib inline
+import matplotlib.pyplot as plt
+# plt.style.use('seaborn-white')
+import numpy as np
+
+
+
 # define the header structure
-header_struct = struct.Struct('<HBBHBBBBHl')
+header_struct = struct.Struct('<HBBHBBBBHi')
 
 
 # define the ping structure
@@ -14,16 +22,44 @@ header_struct = struct.Struct('<HBBHBBBBHl')
 # data_struct = struct.Struct('lLLhhHHHhhhhHHhhhhhffhhhhhhhhhhhhfllhBBBBBBBBBBBBBBBBBBBBBBBBHLHhhHHHllHHlffhhhhhh')
 
 # This includes both the header and the data
-data_struct = struct.Struct('HBBHBBBBHllLLhhHHHhhhhHHhhhhhffhhhhhhhhhhhhfllh')
+data_struct = struct.Struct('HBBHBBBBHiiIIhhHHHhhhhHHhhhhhffhhhhhhhhhhhhfiih')
 #  B = uint8 H = uint16 L = uint32 f = float
 #  b = int8 h = int16 l = int 32 f = float
 
+
+
+def f(x, y):
+    return np.sin(x) ** 10 + np.cos(10 + y * x) * np.cos(x)
+
+
+def graph(long, lat):
+    # x = np.linspace(long)
+    # y = np.linspace(lat)
+    x = long
+    y = lat
+    
+
+    X, Y = np.meshgrid(x, y)
+    Z = f(X, Y)
+    
+    plt.contour(X, Y, Z, colors='black');
+
+
+
+
+
+
+
+
 def main():
+    x = []
+    y = []
     # Goes through your directory to find files
-    directory = 'Immerse/Sidescans/'
+    directory = 'JSF-Processing/Sidescans/'
     for filename in os.listdir(directory):
         if filename.endswith('.JSF'):
             with open(os.path.join(directory, filename), 'rb') as f:
+                sensorCount = 0
                 # read the header
                 test = f.read()
                 for i in range(len(test)):
@@ -35,37 +71,48 @@ def main():
                     #     break
 
                     # This is here for testing to speed up tests
-                    f.seek(0 + i * data_struct.size)
+                    # f.seek(0 + i * data_struct.size)
 
                     # This will be the normal one
-                    # f.seek(0 + i)
+                    f.seek(i)
                     
                     # This takes the data from the file and only gathers the amount that it will to fill the variables
-                    header = f.read(data_struct.size)
+                    if i < 10000000:
+                        header = f.read(data_struct.size)
 
-                    # Takes the data out and assigns it to each variable. This corresponds to the
-                    # sizes shown in the data_struct variable.
-                    marker, protocalVersion, sessionID, messageType, cmdType, subSysNum, channel, \
-                    seqNum, reserved, msgSize, time, startingDepth, pingNumber,reserved1, reserved2, \
-                    MSB, LSB, LSB2, reserved3, reserved4, reserved5, ID, validityFlag, reserved6, dataFormat, \
-                    distanceToTow1, distanceToTow2, reserved7, reserved8, kmOfPipe, heave, reserved9, \
-                    reserved10, reserved11, reserved12, reserved13, reserved14, reserved15, reserved16, \
-                    reserved17, reserved18, reserved19, reserved20, gapFiller, longitude, latitude, coordUnits = data_struct.unpack(header)
+                        # Takes the data out and assigns it to each variable. This corresponds to the
+                        # sizes shown in the data_struct variable.
+                        marker, protocalVersion, sessionID, messageType, cmdType, subSysNum, channel, \
+                        seqNum, reserved, msgSize, time, startingDepth, pingNumber,reserved1, reserved2, \
+                        MSB, LSB, LSB2, reserved3, reserved4, reserved5, ID, validityFlag, reserved6, dataFormat, \
+                        distanceToTow1, distanceToTow2, reserved7, reserved8, kmOfPipe, heave, reserved9, \
+                        reserved10, reserved11, reserved12, reserved13, reserved14, reserved15, reserved16, \
+                        reserved17, reserved18, reserved19, reserved20, gapFiller, longitude, latitude, coordUnits = data_struct.unpack(header)
 
-                    # Every set of data should start with decimal 5633 (0x1601 hex)
-                    if(marker == 5633):
+                        # Every set of data should start with decimal 5633 (0x1601 hex)
+                        if(marker == 5633):
 
-                        # Message type 80 shows data collection
-                        if messageType == 80:
-                            # if coordUnits == 1 or coordUnits == 2 or coordUnits == 3 or coordUnits == 4:
-                                # pass
-                            if(channel):
-                                side = "Starboard"
-                            else:
-                                side = "Port"
-                            # This is just here for testing so I can see the values. Later this will be a graph
-                            print(f"\n\nMarker:{hex(marker)} MSGTYPE:{messageType} Channel:{channel} Side:{side} ")
-                            print(f"Longitude:{longitude} Latitude{latitude} coordUnit:{coordUnits}")
+                            # Message type 80 shows data collection
+                            if messageType == 80:
+                                if coordUnits == 1 or coordUnits == 2 or coordUnits == 3 or coordUnits == 4:
+                                    # pass
+                                    if ID == 1:
+                                        if(channel == 1):
+                                            side = "Starboard"
+                                        elif channel == 0:
+                                            side = "Port"
+                                        else:
+                                            side = "INVALID"
+                                        # This is just here for testing so I can see the values. Later this will be a graph
+                                        print(f"\n\nMarker:{hex(marker)} MSGTYPE:{messageType} Channel:{channel} Side:{side} ")
+                                        print(f"Longitude:{longitude} Latitude{latitude} coordUnit:{coordUnits} ID:{ID}")
+                                        x.append(longitude)
+                                        y.append(latitude)
+                                        sensorCount += 1
+                    else:
+                        print(f"Total Sensor Measurements : {sensorCount}")
+                        graph(x,y)
+                        break
     f.close()
 
 main()
