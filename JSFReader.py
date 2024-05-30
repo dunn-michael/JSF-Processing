54f3205dd846dcd46e1acae1c5058f056e981547
dunn-michael <dunnm2001@gmail.com>
2024-05-30 14:02:04 -0600

    Addede CSV file reader
    Added CSV file reader and writer. Struggling with the size of the rows not being large enough?

--- JSFReader.py
+++ JSFReader.py
@@ -6,75 +6,38 @@ import matplotlib.pyplot as plt
 from tkinter import filedialog
 from tkinter import *
 import math
+import csv
+import sys
 # from histManipulation import *
 
-def setupGraph(listName):
-    imgnew = []
-    for thing in listName:
-        # print(len(thing))
-        if len(thing) == 15602:
-            # Value if graphing single graphs instead of combined
-        # if len(thing) == 7801:
-            imgnew.append(thing)
-    array = np.array(imgnew)
-    
-    # array = equalize_histogram(array, "gaussian")
-    # plt.title("Sonar map")
-    # plt.subplot(3,1,1)
-    plt.imshow(np.fliplr((array).T), cmap='pink', vmin= 0, vmax = 24881)
-    # plt.title("Graph of single swath")
-    # plt.subplot(3,1,2)
-    # plt.plot(array[1])
-    # plt.title("Histogram")
-    # plt.subplot(3,1,3)
-    # plt.hist(array[1], bins=200, color='skyblue', edgecolor='black')
-    # plt.title(channel)
-    # equ
+csv.field_size_limit(sys.maxsize)
 
-
-def main():
-    
+def file_reader():
+    print("---=Gathering Data=---")
     header_struct = struct.Struct('<HBBHBBBBHi')
-    finishedList = []
-
-    echoIntensitiesL = []
-    echoIntensitiesR = []
-    imgL = []
-    imgR = []
-    img = []
-    totalR = []
-    totalL = []
-    prevLong = 0
-    prevLat = 0
-    same = 0
-    skip = False
-
     port = False
     starboard = False
-
-    i = 0
-    count = 0
     directory = 'Sidescans/'
-    account = 0
-    
-    # This is the absorption coefficient use for TVG
-    # alpha = 4.9
     alpha = 7.5
-    # alpha = 
-
-    # TODO
-    # UNCOMMENT LATER
-    # OPTIONAL CAN ISNTEAD USE THE NORMAL DIRECTORY VARIABLE
+    i = 0
+    echoIntensitiesR = []
+    echoIntensitiesL = []
+    echoIntensitiesRev = []
+    heading = 0
+    currentLat = 0
+    currentLong = 0
+    prevLat = 0
+    prevLong = 0
+    account = 0
+    info = []
+    skip = False
     # tkinter.Tk().withdraw() # prevents an empty tkinter window from appearing
     # directory = filedialog.askdirectory()
 
-    # Looks at your directory and finds .JSF files to open and parset through the data
     for filename in os.listdir(directory):
         if filename.endswith('.JSF'):
             imgL = []
-            imgR = []
-            print("IMAGE LISTS RESET")
-            
+            imgR = []            
 
 
             # Prediction value is used to read the message length add 16 bits for the length
@@ -92,7 +55,7 @@ def main():
                 # we won't be able to unpack the header struct so skip it.
                 # The 16 represents the size of the header struct.
                 while prediction <= len(data):
-                    if prediction + 16 >= len(data):
+                    if prediction + 16 + 240 >= len(data):
                         break
                     f.seek(i)
                     header = f.read(header_struct.size)
@@ -101,7 +64,8 @@ def main():
 
                     longitude = struct.unpack('<i', data[i+96:i+96+4])[0] / 10000 / 60
                     latitude =  struct.unpack('<i', data[i+100:i+100+4])[0] / 10000 / 60
-
+                    heading =   struct.unpack('<H', data[i+172+16:i+172+16+2])[0]
+                    # print(heading)
                     # Checks for the beginning of the messages incase the prediction
                     # value lead us astray. Beginning of a message should
                     # always start with x1601 or decimal 5633
@@ -133,18 +97,18 @@ def main():
                                     currentLat = latitude
 
                                     if currentLong == prevLong and currentLat == prevLat:
+                                        port = False
+                                        starboard = False
                                         if channel == 0:
                                             port = True
                                         if channel == 1:
                                             starboard = True
                                         if port and starboard:
-                                            skip = True
                                             port = False
                                             starboard = False
-                                            same += 1
+                                            skip = True
                                         # print(i)
                                         # i = prediction
-
                                     else:
                                         skip = False
                                     prevLat = currentLat
@@ -154,96 +118,167 @@ def main():
                                     account += 1
 
                                     # Channel 0 represents the port side of the vessel
-                                    if channel == 0:
-                                        # j is variable that we are using to index through
-                                        # the echo intensity messages. We can get the size
-                                        # of this by looking at the msgSize variable then adding
-                                        # 240 for all of the information included in the
-                                        # message type 80 message, then we add 16 for the size
-                                        # of the header message
-                                        j = i + 240 + 16
-                                        echoIntensitiesL = []
-                                        
-                                        # j needs to stay less than or equal to the prediction because
-                                        # if j gets larger than the prediction we will begin to look
-                                        # at the next message
-                                        while j <= prediction:
-                                            try: 
-                                                # alpha = 7.5
-                                                val = struct.unpack('<h', data[j:j+2])[0]
+                                    if not skip:
+                                        if channel == 0:
+                                            # j is variable that we are using to index through
+                                            # the echo intensity messages. We can get the size
+                                            # of this by looking at the msgSize variable then adding
+                                            # 240 for all of the information included in the
+                                            # message type 80 message, then we add 16 for the size
+                                            # of the header message
+                                            j = i + 240 + 16
+                                            echoIntensitiesL = []
+                                            
+                                            # j needs to stay less than or equal to the prediction because
+                                            # if j gets larger than the prediction we will begin to look
+                                            # at the next message
+                                            while j <= prediction:
+                                                try: 
+                                                    # alpha = 7.5
+                                                    val = struct.unpack('<h', data[j:j+2])[0]
 
-                                                val = val / 2
-                                                val = 40 * math.log10(val + 1) + alpha * val
-                                                # val = math.sqrt( (val / 2)**2)
-                                                j += 2
-                                                echoIntensitiesL.append(val)     
-                                            except:
-                                                break
+                                                    val = val / 2
+                                                    val = 40 * math.log10(val + 1) + alpha * val
+                                                    # val = math.sqrt( (val / 2)**2)
+                                                    j += 2
+                                                    echoIntensitiesL.append(val)     
+                                                except:
+                                                    break
 
-                                    # Channel 1 represents the starboard side of the vessel
-                                    if channel == 1:
-                                        j = i + 240 + 16
-                                        # h = (struct.unpack('<i', data[i+136:i+136+4])[0])
-                                        # depth = (struct.unpack('<i', data[i+136+16:i+16+136+4])[0]) * 10 **(-3)
-                                        # validityFlag = (struct.unpack('<H', data[i+30+16:i+16+30+2])[0])
-                                        h = (struct.unpack('<i', data[i+144+16:i+16+144+4])[0]) * 10 ** -3
-                                        echoIntensitiesR = []
-                                        while j <= prediction:
-                                            try: 
-                                                val = struct.unpack('<h', data[j:j+2])[0]
-                                                val = val / 2
-                                                # alpha = 4
-                                                val = 40 * math.log10(val + 1) + alpha * val
-                                                # print(val)
-                                                # val = math.sqrt( (val / 2)**2 - (h)**2 )
-                                                # val = math.sqrt(((val / 2) ** 2) - ((h)**2))
-                                                # if (val/2) **2 > h**2:
-                                                    # val = math.sqrt((val / 2) ** 2) - ((h)**2)
-                                                # val = math.sqrt( (val / 2)**2)
-                                                # if(val <= 0):
-                                                    # print(f"val : {val}")
+                                        # Channel 1 represents the starboard side of the vessel
+                                        if channel == 1:
+                                            j = i + 240 + 16
+                                            # h = (struct.unpack('<i', data[i+136:i+136+4])[0])
+                                            # depth = (struct.unpack('<i', data[i+136+16:i+16+136+4])[0]) * 10 **(-3)
+                                            # validityFlag = (struct.unpack('<H', data[i+30+16:i+16+30+2])[0])
+                                            h = (struct.unpack('<i', data[i+144+16:i+16+144+4])[0]) * 10 ** -3
+                                            echoIntensitiesR = []
+                                            while j <= prediction:
+                                                try: 
+                                                    val = struct.unpack('<h', data[j:j+2])[0]
+                                                    val = val / 2
+                                                    # alpha = 4
+                                                    val = 40 * math.log10(val + 1) + alpha * val
+                                                    # print(val)
+                                                    # val = math.sqrt( (val / 2)**2 - (h)**2 )
+                                                    # val = math.sqrt(((val / 2) ** 2) - ((h)**2))
+                                                    # if (val/2) **2 > h**2:
+                                                        # val = math.sqrt((val / 2) ** 2) - ((h)**2)
+                                                    # val = math.sqrt( (val / 2)**2)
+                                                    # if(val <= 0):
+                                                        # print(f"val : {val}")
 
-                                                j += 2
-                                                echoIntensitiesR.append(val)
-                                            except:
-                                                break
+                                                    j += 2
+                                                    echoIntensitiesR.append(val)
+                                                except:
+                                                    break
                                 # Set the index to the next prediction value so that
                                 # once we are done getting the echo intensity information
                                 # we can move onto the next message.
                                 i = prediction
-
-                                # Count is the variable used to show how many headers we are sorting through
-                                count += 1
-                                echoIntensitiesRev = list(reversed(echoIntensitiesR))
-                                if len(echoIntensitiesL) != 0 and len(echoIntensitiesRev) != 0:
-                                    imgL.append(echoIntensitiesL)
-                                    imgR.append(echoIntensitiesRev)
+                                if not skip:
+                                    echoIntensitiesRev = list(reversed(echoIntensitiesR))
+                                    if len(echoIntensitiesL) != 0 and len(echoIntensitiesRev) != 0:
+                                        imgL.append(echoIntensitiesL)
+                                        imgR.append(echoIntensitiesRev)
+                                        info.append([latitude, longitude,heading,0,imgL[-1]])
+                                        # data.append([latitude,longitude,1, imgR[-1]])
+                                        info.append([latitude,longitude,heading,1,imgR[-1]])
+                                    skip = False
                                 # totalL.append(echoIntensitiesL)
                                 # totalR.append(echoIntensitiesRev)
-                            
-                print(f"finished file : {filename}")
-                finishedList.append(filename)
-                print(f"Count : {count}")
-                print(f"Msg 80 count: {account}")
 
                 f.close()
                 # splitup = filename.split(".")
                 # filename = splitup[0] + ".png"
                 # plt.savefig(filename)
             # Comment out to go through all files
-            break
+            # break
+    header = ['Latitude','Longitude', 'Heading', 'Channel', 'Data']
+    with open('graph-data.csv', 'w') as csvFile:
+        csvwriter = csv.writer(csvFile)
+        csvwriter.writerow(header)
+        csvwriter.writerows(info)
+        csvFile.close()
+    print("---=Finished Gathering Data=---")
     # print(f"Length R:{len(totalR)}")
     # print(f"Length L:{len(totalL)}")
-    img = [[] for _ in range(len(imgL))]
+    # img = [[] for _ in range(len(imgL))]
     # print(f"combined:{len(img)}")
 
-    for i in range(len(imgL)):
-        # img[i] = imgR[i] + imgL[i]
-        img[i] = imgR[i] + imgL[i]
+    # for i in range(len(imgL)):
+    #     # img[i] = imgR[i] + imgL[i]
+    #     img[i] = imgR[i] + imgL[i]
     # print(f"ImgR length:{len(imgR)}")
     # print(f"ImgL length:{len(imgL)}")
     # print(f"Total length:{len(imgR[1])}")
-    print(same)
+
+def sort_data():
+    print("---=Sorting=---")
+    info = []
+    prevLat = 0
+    prevLong = 0
+    currentLat = 0
+    currentLong = 0
+    starboard = False
+    skip = False
+    with open('graph-data.csv', 'r') as csvFile:
+        csvreader = csv.reader(csvFile)
+        header = next(csvreader)
+        for row in csvreader:
+            currentLat = row[0]
+            currentLong = row[1]
+            if currentLat == prevLat and currentLong == prevLong:
+                port = False
+                starboard = False
+                if row[2] == 0:
+                    port = True
+                if row[2] == 1:
+                    starboard == True
+                if starboard and port:
+                    skip = True
+                    starboard = False
+                    port = False
+                else:
+                    starboard = False
+                    port = False
+            if not skip:
+                info.append(row[4])
+        csvFile.close()
+    print("---=Finished Sorting=---")
+    return info
+
+
+def setupGraph(listName):
+    imgnew = []
+    for thing in listName:
+        # print(len(thing))
+        if len(thing) == 15602:
+            # Value if graphing single graphs instead of combined
+        # if len(thing) == 7801:
+            # imgnew.append(thing)
+            print(True)
+    array = np.array(imgnew)
+    
+    # array = equalize_histogram(array, "gaussian")
+    # plt.title("Sonar map")
+    # plt.subplot(3,1,1)
+    plt.imshow(np.fliplr((array).T), cmap='pink', vmin= 0, vmax = 24881)
+    # plt.title("Graph of single swath")
+    # plt.subplot(3,1,2)
+    # plt.plot(array[1])
+    # plt.title("Histogram")
+    # plt.subplot(3,1,3)
+    # plt.hist(array[1], bins=200, color='skyblue', edgecolor='black')
+    # plt.title(channel)
+    # equ
+
+
+def main():
+    
+    # file_reader()
+    img = sort_data()
+    # print(same)
     setupGraph(img)
     plt.show()
 
