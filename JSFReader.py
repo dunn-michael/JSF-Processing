eedd346e2ff9146832cd932254eaedeaabdaf3b8
dunn-michael <dunnm2001@gmail.com>
2024-05-30 07:29:50 -0600

    Got it working

--- JSFReader.py
+++ JSFReader.py
@@ -49,6 +49,9 @@ def main():
     same = 0
     skip = False
 
+    port = False
+    starboard = False
+
     i = 0
     count = 0
     directory = 'Sidescans/'
@@ -98,121 +101,125 @@ def main():
 
                     longitude = struct.unpack('<i', data[i+96:i+96+4])[0] / 10000 / 60
                     latitude =  struct.unpack('<i', data[i+100:i+100+4])[0] / 10000 / 60
-                    # longitude, latitude = struct.unpack('<ii', data[i+96:i+96+8])[0]
-                    # print(longitude)
-                    # print(latitude)
-                    currentLong = longitude
-                    currentLat = latitude
 
-                    if currentLong == prevLong and currentLat == prevLat:
-                        same += 1
-                        skip = True
-                    else:
-                        skip = False
-                    prevLat = currentLat
-                    prevLong = currentLong
                     # Checks for the beginning of the messages incase the prediction
                     # value lead us astray. Beginning of a message should
                     # always start with x1601 or decimal 5633
                     # Protocal version should also stay 14 for the
                     # time being so its there as another sanity check
-
-                    if not skip:
-                        if marker == 5633 and protocalVersion == 14:
+                    if marker == 5633 and protocalVersion == 14:
+                            
+                            # Any message that shows a type larger than 2111 isn't included in the documentation
+                            # and messes up the prediction so it is being ignored
+                            if(messageType > 2112):
+                                pass
+                            else:
                                 
-                                # Any message that shows a type larger than 2111 isn't included in the documentation
-                                # and messes up the prediction so it is being ignored
-                                if(messageType > 2112):
-                                    pass
-                                else:
+                                # Prediction size is calculated by taking the message size
+                                # and adding 16 to it for the size of the header then has
+                                # i added to it since i represents the current index
+                                # this allows us to skip directly to the header of the next
+                                # message 
+                                prediction = i + msgSize + 16
 
-                                    # Prediction size is calculated by taking the message size
-                                    # and adding 16 to it for the size of the header then has
-                                    # i added to it since i represents the current index
-                                    # this allows us to skip directly to the header of the next
-                                    # message 
-                                    prediction = i + msgSize + 16
+                                # The message type that we are looking for is 80
+                                # this is the standard message that has a ton of
+                                # important information that we can use
+                                # subSysNum 20 represents the low frequency data
+                                # If there comes a time where we wish to also graph
+                                # high frequency data we can change the 20 to a 21
+                                if(messageType == 80) and subSysNum == 20:
+                                    currentLong = longitude
+                                    currentLat = latitude
 
-                                    # The message type that we are looking for is 80
-                                    # this is the standard message that has a ton of
-                                    # important information that we can use
-                                    # subSysNum 20 represents the low frequency data
-                                    # If there comes a time where we wish to also graph
-                                    # high frequency data we can change the 20 to a 21
-                                    if(messageType == 80) and subSysNum == 20:
-
-                                        # account is being incremented to count the amount of
-                                        # acoustic information we are parsing through
-                                        account += 1
-
-                                        # Channel 0 represents the port side of the vessel
+                                    if currentLong == prevLong and currentLat == prevLat:
                                         if channel == 0:
-
-                                            # j is variable that we are using to index through
-                                            # the echo intensity messages. We can get the size
-                                            # of this by looking at the msgSize variable then adding
-                                            # 240 for all of the information included in the
-                                            # message type 80 message, then we add 16 for the size
-                                            # of the header message
-                                            j = i + 240 + 16
-                                            echoIntensitiesL = []
-                                            
-                                            # j needs to stay less than or equal to the prediction because
-                                            # if j gets larger than the prediction we will begin to look
-                                            # at the next message
-                                            while j <= prediction:
-                                                try: 
-                                                    # alpha = 7.5
-                                                    val = struct.unpack('<h', data[j:j+2])[0]
-
-                                                    val = val / 2
-                                                    val = 40 * math.log10(val + 1) + alpha * val
-                                                    # val = math.sqrt( (val / 2)**2)
-                                                    j += 2
-                                                    echoIntensitiesL.append(val)     
-                                                except:
-                                                    break
-
-                                        # Channel 1 represents the starboard side of the vessel
+                                            port = True
                                         if channel == 1:
-                                            j = i + 240 + 16
-                                            # h = (struct.unpack('<i', data[i+136:i+136+4])[0])
-                                            # depth = (struct.unpack('<i', data[i+136+16:i+16+136+4])[0]) * 10 **(-3)
-                                            # validityFlag = (struct.unpack('<H', data[i+30+16:i+16+30+2])[0])
-                                            h = (struct.unpack('<i', data[i+144+16:i+16+144+4])[0]) * 10 ** -3
-                                            echoIntensitiesR = []
-                                            while j <= prediction:
-                                                try: 
-                                                    val = struct.unpack('<h', data[j:j+2])[0]
-                                                    val = val / 2
-                                                    # alpha = 4
-                                                    val = 40 * math.log10(val + 1) + alpha * val
-                                                    # print(val)
-                                                    # val = math.sqrt( (val / 2)**2 - (h)**2 )
-                                                    # val = math.sqrt(((val / 2) ** 2) - ((h)**2))
-                                                    # if (val/2) **2 > h**2:
-                                                        # val = math.sqrt((val / 2) ** 2) - ((h)**2)
-                                                    # val = math.sqrt( (val / 2)**2)
-                                                    # if(val <= 0):
-                                                        # print(f"val : {val}")
+                                            starboard = True
+                                        if port and starboard:
+                                            skip = True
+                                            port = False
+                                            starboard = False
+                                            same += 1
+                                        # print(i)
+                                        # i = prediction
 
-                                                    j += 2
-                                                    echoIntensitiesR.append(val)
-                                                except:
-                                                    break
-                                    # Set the index to the next prediction value so that
-                                    # once we are done getting the echo intensity information
-                                    # we can move onto the next message.
-                                    i = prediction
+                                    else:
+                                        skip = False
+                                    prevLat = currentLat
+                                    prevLong = currentLong
+                                    # account is being incremented to count the amount of
+                                    # acoustic information we are parsing through
+                                    account += 1
 
-                                    # Count is the variable used to show how many headers we are sorting through
-                                    count += 1
-                                    echoIntensitiesRev = list(reversed(echoIntensitiesR))
-                                    # if len(echoIntensitiesL) != 0 and len(echoIntensitiesRev) != 0:
-                                    # imgL.append(echoIntensitiesL)
-                                    # imgR.append(echoIntensitiesRev)
-                                    totalL.append(echoIntensitiesL)
-                                    totalR.append(echoIntensitiesRev)
+                                    # Channel 0 represents the port side of the vessel
+                                    if channel == 0:
+                                        # j is variable that we are using to index through
+                                        # the echo intensity messages. We can get the size
+                                        # of this by looking at the msgSize variable then adding
+                                        # 240 for all of the information included in the
+                                        # message type 80 message, then we add 16 for the size
+                                        # of the header message
+                                        j = i + 240 + 16
+                                        echoIntensitiesL = []
+                                        
+                                        # j needs to stay less than or equal to the prediction because
+                                        # if j gets larger than the prediction we will begin to look
+                                        # at the next message
+                                        while j <= prediction:
+                                            try: 
+                                                # alpha = 7.5
+                                                val = struct.unpack('<h', data[j:j+2])[0]
+
+                                                val = val / 2
+                                                val = 40 * math.log10(val + 1) + alpha * val
+                                                # val = math.sqrt( (val / 2)**2)
+                                                j += 2
+                                                echoIntensitiesL.append(val)     
+                                            except:
+                                                break
+
+                                    # Channel 1 represents the starboard side of the vessel
+                                    if channel == 1:
+                                        j = i + 240 + 16
+                                        # h = (struct.unpack('<i', data[i+136:i+136+4])[0])
+                                        # depth = (struct.unpack('<i', data[i+136+16:i+16+136+4])[0]) * 10 **(-3)
+                                        # validityFlag = (struct.unpack('<H', data[i+30+16:i+16+30+2])[0])
+                                        h = (struct.unpack('<i', data[i+144+16:i+16+144+4])[0]) * 10 ** -3
+                                        echoIntensitiesR = []
+                                        while j <= prediction:
+                                            try: 
+                                                val = struct.unpack('<h', data[j:j+2])[0]
+                                                val = val / 2
+                                                # alpha = 4
+                                                val = 40 * math.log10(val + 1) + alpha * val
+                                                # print(val)
+                                                # val = math.sqrt( (val / 2)**2 - (h)**2 )
+                                                # val = math.sqrt(((val / 2) ** 2) - ((h)**2))
+                                                # if (val/2) **2 > h**2:
+                                                    # val = math.sqrt((val / 2) ** 2) - ((h)**2)
+                                                # val = math.sqrt( (val / 2)**2)
+                                                # if(val <= 0):
+                                                    # print(f"val : {val}")
+
+                                                j += 2
+                                                echoIntensitiesR.append(val)
+                                            except:
+                                                break
+                                # Set the index to the next prediction value so that
+                                # once we are done getting the echo intensity information
+                                # we can move onto the next message.
+                                i = prediction
+
+                                # Count is the variable used to show how many headers we are sorting through
+                                count += 1
+                                echoIntensitiesRev = list(reversed(echoIntensitiesR))
+                                if len(echoIntensitiesL) != 0 and len(echoIntensitiesRev) != 0:
+                                    imgL.append(echoIntensitiesL)
+                                    imgR.append(echoIntensitiesRev)
+                                # totalL.append(echoIntensitiesL)
+                                # totalR.append(echoIntensitiesRev)
                             
                 print(f"finished file : {filename}")
                 finishedList.append(filename)
@@ -224,15 +231,15 @@ def main():
                 # filename = splitup[0] + ".png"
                 # plt.savefig(filename)
             # Comment out to go through all files
-            # break
-    print(f"Length R:{len(totalR)}")
-    print(f"Length L:{len(totalL)}")
-    img = [[] for _ in range(len(totalL))]
-    print(f"combined:{len(img)}")
+            break
+    # print(f"Length R:{len(totalR)}")
+    # print(f"Length L:{len(totalL)}")
+    img = [[] for _ in range(len(imgL))]
+    # print(f"combined:{len(img)}")
 
-    for i in range(len(totalL)):
+    for i in range(len(imgL)):
         # img[i] = imgR[i] + imgL[i]
-        img[i] = totalR[i] + totalL[i]
+        img[i] = imgR[i] + imgL[i]
     # print(f"ImgR length:{len(imgR)}")
     # print(f"ImgL length:{len(imgL)}")
     # print(f"Total length:{len(imgR[1])}")
